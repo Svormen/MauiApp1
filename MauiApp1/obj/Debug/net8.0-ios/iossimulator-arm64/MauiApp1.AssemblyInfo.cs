0ebd4ba929a70b14d3b405a401e2cd31ce651e6f
Svormen <62176082+Svormen@users.noreply.github.com>
2025-03-13 09:37:10 +0100

    changed name of function

--- MauiApp1/obj/Debug/net8.0-ios/iossimulator-arm64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net8.0-ios/iossimulator-arm64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6ae109cc7092f44c2d02fd3d94669215f01637d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d1b35dbb31ccfb2580392496cc945c91ae604748")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
