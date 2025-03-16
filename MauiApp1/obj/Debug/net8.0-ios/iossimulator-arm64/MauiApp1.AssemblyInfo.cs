a00969f20d8ac6cc9a0f61373d71546e3c94f1dc
Svormen <62176082+Svormen@users.noreply.github.com>
2025-03-16 20:44:30 +0100

    adding table for game

--- MauiApp1/obj/Debug/net8.0-ios/iossimulator-arm64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net8.0-ios/iossimulator-arm64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5d6e40cabf946c68b38c223a9a5674184a868ff8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+515aa34118f671a58a854f4828ff6bc8666e669c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
